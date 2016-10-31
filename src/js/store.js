3d20784d61b787e5f11073fe83153c6970b07977
Joseph DeSimpliciis <jcdesimp@gmail.com>
2016-10-31 15:49:14 -0400

    DevTools hooked up.

--- src/js/store.js
+++ src/js/store.js
@@ -1,10 +1,13 @@
 "use strict"
 
 import { createStore, applyMiddleware, compose } from 'redux';
+import DevTools from './components/devTools/devTools';
 
 import reducer from './reducers';
 
 let enhancements = [  ];
+process.env.NODE_ENV === 'production' ? null : enhancements.push(DevTools.instrument());
+
 
 const enhancer = compose(
   ...enhancements
