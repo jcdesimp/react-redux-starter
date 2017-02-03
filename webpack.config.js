ecb3d02b77daa0dd32d2a937ad5a4361f4d7e03a
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-02-02 20:27:58 -0500

    Add airbnb-linter, es2016 support.

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,77 @@
-"use strict";
 /* global process */
 
-const ProjectPackage        = require('./package');
-const webpack               = require('webpack');
-const path                  = require("path");
-const HtmlWebpackPlugin     = require('html-webpack-plugin');
-const ExtractTextPlugin     = require('extract-text-webpack-plugin');
+const ProjectPackage = require('./package.json');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: './bin',
-        filename: 'bundle.js'
-    },
+  entry: './src/index.js',
+  output: {
+    path: './bin',
+    filename: 'bundle.js',
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.jsx|\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015'],
-                    plugins: ["transform-react-jsx"]
-                }
-            },
-            {
-                test: /\.html?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.(css|scss)$/,
-                loader: ExtractTextPlugin.extract("style", "css!sass")
-            },
-            { 
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=8192'
-            },
-            {
-                test: /\.(md|MD)$/,
-                loader: 'html!markdown?gfm=true'
-            }
-        ]
-    },
-    plugins: [
-        // new webpack.optimize.UglifyJsPlugin({
-        //  compress: {
-        //      warnings: false,
-        //  },
-        //  output: {
-        //      comments: false
-        //  }
-        // }),
-        new HtmlWebpackPlugin({
-            title: ProjectPackage.name,
-            template: './src/index.ejs',
-            inject: false
-        }),
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-            }
-        }),
-        new ExtractTextPlugin("[name].css")
+  module: {
+    loaders: [
+      {
+        test: /\.jsx|\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2016', 'es2015'],
+          plugins: ['transform-react-jsx'],
+        },
+      },
+      {
+        test: /\.html?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass'),
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192',
+      },
+      {
+        test: /\.(md|MD)$/,
+        loader: 'html!markdown?gfm=true',
+      },
     ],
-    resolve: {
-        extensions: ["", ".js", ".jsx", ".md", ".MD"],
-        alias: {
-            "css": path.resolve(__dirname, "./src/css"),
-            "img": path.resolve(__dirname, "./src/img"),
-            "lib": path.resolve(__dirname, "./src/js/lib"),
-            "modules": path.resolve(__dirname, "./src/js/modules")
-        }
-    }
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({
+    //  compress: {
+    //      warnings: false,
+    //  },
+    //  output: {
+    //      comments: false
+    //  }
+    // }),
+    new HtmlWebpackPlugin({
+      title: ProjectPackage.name,
+      template: './src/index.ejs',
+      inject: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new ExtractTextPlugin('[name].css'),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.md', '.MD'],
+    alias: {
+      css: path.resolve(__dirname, './src/css'),
+      img: path.resolve(__dirname, './src/img'),
+      lib: path.resolve(__dirname, './src/js/lib'),
+      modules: path.resolve(__dirname, './src/js/modules'),
+    },
+  },
 };
